4106476b1883754252b029d5b90d6ff1692055fb
Garet <kellergaret@outlook.com>
2018-05-05 19:35:43 -0600

    Finalized JS and HTML

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Word-Guess-Game
-03-Homework  Javascript
+# JavaScript Assignment
+
+### Overview
+
+In this game of Hangman,  the player will use the keyboard run Hangman in the browser. Javascript will power the game and cause changes dynamically in the HTML.
+
+The number of games won will be tracked and during the game the number of guesses left will be tracked and updated alongwith displaying the letters guessed incorrectly.
+The word will be changed automatically after the game is finished. Players need to wait for the game to fully reset(time to wait can be adjusted.
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,209 +1,183 @@
 
 //Variables
 
-var availableLetters = "abcdefghijklmnopqrstuvwxyz";
-//var availableLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var availableLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessedLetters = [];
-var lives = 13;
-var words = ["wookie", "droid", "speeder", "starship"];
+var lives = 10;
+var words = ["wookie", "droid", "speeder", "starship", "starfighter","planet", "shuttle", "walker", "trooper", "transport", "astromech", "squadron", "blaster", "pilot", ];
 var currentWord = [];
-var guessed= "already guessed, please try again...";
-var wrongLetter= "Please enter a letter from A-Z";
+var wrongLetter= "Only keys A-Z are valid to enter as guesses";
 var gameOver = true;
+var win = 0;
+var mySound;
+var loseSound;
+var myMusic;
+var invalidSong;
 
-function gameReset(){
-    lives = 13;
-//var guessedLetters = [];
-};
-
-var wordChosen = words[Math.floor(Math.random() * words.length)];    
+//loads music and choses first word for the game with approriate number of spaces, upon the loading of the page
+window.onload = function() {
+  myMusic = new sound("star-wars-cantina-song.mp3");
+  
+wordChosen = words[Math.floor(Math.random() * words.length)];    
 console.log(wordChosen); console.log(wordChosen.length);
 for(var j = 0; j < wordChosen.length; j++){
     currentWord.push(" + ");
-    document.getElementById("currentWord").innerHTML = currentWord;}
-
-    //Choose Random Word....Setup this function to by activated by onkeyup but only if gameover=true
-document.onkeyup = function(event){
-   
-    
-   
-    
-    
-    if(wordChosen.length > 0);{
-     userGuess = event.key.toLowerCase();
-    //console.log(userGuess);
-
+    document.getElementById("currentWord").innerHTML = currentWord.join("");}
+    document.querySelector("#gameStats").innerHTML = "Guesses left: " + lives;
+    document.querySelector("#totalWins").innerHTML = "Words Guessed Correctly: " + win;
 }
-    //Generate blanks based on length of chosen word
-
-    //var gameStart = gameSetup;
 
     
+//begins preparation for next game by increasing total wins and refreshing game arrays 
+function gameReset(){
+  if(gameOver = true){
+    lives = 10;
+guessedLetters = [];
+ gameOver = false;
+ currentWord = [];
+ document.querySelector("#totalWins").innerHTML = "Words Guessed Correctly: " + win;
+ wordChosen =("");
+ availableLetters = [];      
+ gameStart();
+ console.log(guessedLetters);
+ console.log(availableLetters);
+ console.log(wordChosen);
+ console.log(currentWord);
+}}
+
+//clears last game status message signalling the game is now ready for the next round
+function messageClear() {setTimeout(function(){ 
+  document.getElementById("gameOutcome").style.display = "none";
+}, 500);
+}
+
+//Chooses new word and matches spaces for it. After this the word is ready to be guessed after message is cleared
+function gameStart(){
+wordChosen = words[Math.floor(Math.random() * words.length)];    
+console.log(wordChosen); console.log(wordChosen.length);
+for(var j = 0; j < wordChosen.length; j++){
+    currentWord.push(" + ");
+    document.getElementById("currentWord").innerHTML = currentWord.join("");}
+    document.querySelector("#gameStats").innerHTML = "Guesses left: " + lives;
+    document.querySelector("#guessedWords").innerHTML = guessedLetters;
+    availableLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    console.log(currentWord);
+    console.log(availableLetters);
+    messageClear();
+}
+
+
+    //Choose Random Word, Hide Instructions, begin BGM Music
+document.onkeyup = function(event){
+  myMusic.loop = true;
+  myMusic.play(); 
+  document.getElementById("instructions").style.display = "none";  
+  
+    //Players Guess
+     userGuess = event.key.toLowerCase();
     
-
-
-
-
-//Guessing Begins and correct letters replace blanks
-//document.onkeyup = function(event) {
-   // if(userGuess.indexOf(guessedLetters) < 0);{    
-     //   guessedLetters.push(userGuess);
-    //availableLetters.splice(availableLetters.indexOf(userGuess),1); console.log(guessedLetters);
-//console.log(availableLetters);}
-    
-
- 
-   
-
-//Takes from the array of available letters and pushes the Guessed letter into the Guest Letters Array.
-    //if(guessedLetters.indexOf(userGuess) < 0);{    
-     //   guessedLetters.push(userGuess);
-    //availableLetters.splice(availableLetters.indexOf(userGuess),1); console.log(guessedLetters);
-//console.log(availableLetters);} 
-
-
-
-    
-
-
-   
-    document.getElementById("guessedWords").innerHtml = guessedLetters;
-    
-
-
-//compares user pressed key and replaces a blank if it's correct
-
-
-
-
-    
-//else if(wordChosen.indexOf(userGuess) > 0);{
-    
-//function wordCheck(){
-    //while (wordChosen.indexOf(userGuess) < 0);{ 
-        
-//else {userGuess = correctLetter;}
-
-
-
-switch (wordChosen.indexOf(userGuess)>= 0){ 
+//Validates that key press is a letter and that the pressed key is also in the availableLetters Array
+switch (event.keyCode >= 65 && event.keyCode <= 90&& availableLetters.indexOf(userGuess) >= 0) {
+   case true:   validLetter = userGuess; 
+  
+  //if valid letter matches available letter array run the splice command
+       if(availableLetters.indexOf(validLetter) >= 0){
+     
+      availableLetters.splice(availableLetters.indexOf(validLetter),1);
+   console.log(availableLetters);
+  console.log(validLetter);} 
+  
+switch (wordChosen.indexOf(validLetter)>= 0){ 
    case false: lifeLose(); 
-       
-        console.log(userGuess);
+   guessedLetters.push(userGuess);
+   guessedLetters.sort();
+   
+   console.log(guessedLetters);
+   
+   
+   document.querySelector("#guessedWords").innerHTML = guessedLetters.join(" ");
+   document.querySelector("#gameStats").innerHTML = "Guesses left: " + lives;
+        console.log(validLetter);
         
 //matches the index of the Selected word against player pressed key and checks for multiple instances and places the letter in their accurate spots
- 
-
-
 
 case true: 
 
     for(var i = 0; i < wordChosen.length; i++){
-        if(wordChosen[i] === userGuess){
-        currentWord[i] = userGuess;
+        if(wordChosen[i] === validLetter){
+        currentWord[i] = validLetter;
         console.log(currentWord);
         document.querySelector("#currentWord").innerHTML = currentWord.join("");
-        }}
-    
-          
+        }} 
       }
-    if(lives > 0 && currentWord.indexOf(" + ") <0 ) {
-gameWin();
+      
+    if(lives > 0 && currentWord.indexOf(" + ") <0 ) { gameWin();
     }
-    if(lives <= 0){ gameLose();}
-    
-    }
-    
-  
-    
-    
+    if(lives <= 0){ 
+    gameLose();}
+        }
         
-     
-        
-        //(guessedLetters.indexOf(userGuess) > 0) 
-        
-        
-        
-        //(wordChosen.indexOf(userGuess) < 0); lives -= 1;
-        //console.log(lives);
-            
-        
-
-   
-    
-
-
-   
-
+        if(event.keyCode < 65 || event.keyCode > 90){
+          invalidSong = new sound("swluke01.mp3");
+          invalidSong.play();
+          invalid();
+        }
+      }
     
     function lifeLose(){
         lives -= 1;
         console.log(lives);
             }
 
-    function verify() {}               
-               
-
-            //if(validLetter !== wordChosen.includes(userGuess));{
-              //  var wrongGuess = validLetter;
-                //guessedLetters.push(wrongGuess);
-                //console.log(guessedLetters);
-   
-        //if(wordChosen.indexOf(userGuess) < 0);{
-            //life--;
-    
-    
-   // if(availableLetters.indexOf(userGuess) == 0); {
-     //   alert(guessed);
-      //  console.log(availableLetters.indexOf(userGuess))
-    
-
-
-                                        
-  
-
-
-
-  
-    console.log(availableLetters);
-
     function gameWin(){
         gameOver = true;
-        alert("You win!");
-        gameReset();
+        document.getElementById("gameOutcome").style.display = "block";
+        document.querySelector("#gameOutcome").innerHTML = "You Win!";
+        win += 1;
+        myMusic.stop();
+        mySound = new sound("forcestrong.mp3");
+        mySound.play();
+        document.getElementById("gameOutcome").style.display = "block";
+        document.querySelector("#gameOutcome").innerHTML = "You Win! \n Game Resetting";
+        answer();
     }
+
+  function answer() {
+    setTimeout(function(){ gameReset(); }, 5000);
+}
 
     function gameLose() {
         gameOver = true;
-        alert("Game over! " + wordChosen.toUpperCase() + " is the Correct Word!");
-        gameReset();
-        document.querySelector("#currentWord").innerHTML = wordChosen.join("");
+        myMusic.stop();        
+        loseSound = new sound("laughfuzzball.mp3");
+        loseSound.play();
+        document.getElementById("gameOutcome").style.display = "block";
+    document.querySelector("#gameOutcome").innerHTML = "You Lose! \n Game Resetting";        
+        document.querySelector("#currentWord").innerHTML = wordChosen.valueOf();
+        answer();
     }
-    
-
-   
+    //function to load sound and create a controller in html and hide it from view
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
   
+  document.body.appendChild(this.sound);
+  this.play = function(){
+      this.sound.play();
+  }
+  this.stop = function(){
+      this.sound.pause();
+  }    
+}
 
-
-//var guess = wordChosen.match(userGuess);
-//console.log(guess)
-
-
-
-
-
-
-
-//document.getElementById("currentWord").innerHTML = wordChosen;
-
-//Is Guess found in The Chosen Word-Needs a For loop to check for various instances of the same letter
-
-
-
-//(mainStr.split(",").length - 1)
-
-
-
+function invalid() {
+  setTimeout(function(){alert(wrongLetter);
+  }, 500);
+}
 
 
 
